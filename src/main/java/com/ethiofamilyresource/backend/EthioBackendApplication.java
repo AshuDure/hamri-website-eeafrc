0fedf33a34088c348721f4a7a1d1abe830b3a54b
ashenafi <ashenafi.maru2020@gmail.com>
2024-02-08 18:59:16 -0500

    start backend

---
+++ src/main/java/com/ethiofamilyresource/backend/EthioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ethiofamilyresource.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethiofamilyresource/backend/EthioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethiofamilyresource.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

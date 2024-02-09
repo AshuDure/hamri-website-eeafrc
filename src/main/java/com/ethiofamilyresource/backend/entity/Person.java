3527a702411ef1f8f1d776303017f6f0f33d611b
ashenafi <ashenafi.maru2020@gmail.com>
2024-02-08 22:17:09 -0500

    entity person created

---
+++ src/main/java/com/ethiofamilyresource/backend/entity/Person.java
@@ -0,0 +1,34 @@
+package com.ethiofamilyresource.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String email;
+
+    @Column(name = "phone_number")
+    private Long phoneNumber;
+
+
+}
